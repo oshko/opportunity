ebe31f91129e49c2254972819214354a98e262a4
John Kern <kern3020@gmail.com>
2012-11-07 16:03:39 -0800

    template additions

---
+++ opportunity/tracker/admin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       untitled.py
+#       
+#       Copyright 2012 John Kern 
+#       
+
+from django.contrib import admin
+from tracker.models import UserProfile
+
+admin.site.register(UserProfile)
