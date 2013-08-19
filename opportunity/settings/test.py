e707bb74cab4110e9d19b6bf3625a05cc8bf088e
John Kern <kern3020@gmail.com>
2013-08-19 08:09:35 -0700

    clean up on issue #19

--- opportunity/settings/test.py
+++ opportunity/settings/test.py
@@ -28,7 +28,7 @@ EMAIL_PORT = 1025
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'opportunity',                      # Or path to database file if using sqlite3.
+        'NAME': 'test',                      # Or path to database file if using sqlite3.
         'USER': 'jkern',                      # Not used with sqlite3.
         'PASSWORD': 'password',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -36,7 +36,4 @@ DATABASES = {
     }
 }
 
-# INSTALLED_APPS += ("debug_toolbar",)
-# INTERNAL_IPS = ("127.0.0.1",)
-# MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
