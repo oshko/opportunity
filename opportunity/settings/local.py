edf116a40a81e0b3f03dc4a57aed664b00e98db1
John Kern <kern3020@gmail.com>
2013-08-22 07:41:43 -0700

    user runserver to serve up static files during development

--- opportunity/settings/local.py
+++ opportunity/settings/local.py
@@ -25,6 +25,9 @@ TEMPLATE_DEBUG = DEBUG
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
 
+# override value in base for debugging. 
+STATIC_URL = '/static/'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
