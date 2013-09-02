91f93ec815dbbfe285c6ed489b2f26d8c952ae4c
John Kern <kern3020@gmail.com>
2013-09-02 15:29:29 -0700

    file for production settings

---
+++ opportunity/settings/prod.py
@@ -0,0 +1,23 @@
+import dj_database_url
+import os
+
+from .base import *
+
+# Parse database configuration from $DATABASE_URL
+DATABASES = { 'default': {} } 
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
